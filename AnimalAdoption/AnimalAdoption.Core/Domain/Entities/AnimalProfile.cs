966e42dd8041db09bc19cf8bc9725c9b75cb4ebc
savchenkoadd <savchenkooad@gmail.com>
2023-11-26 19:03:45 +0200

    Added base entity class

---
+++ AnimalAdoption/AnimalAdoption.Core/Domain/Entities/AnimalProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalAdoption.Core.Domain.Entities
+{
+	public class AnimalProfile
+	{
+		public int? Age { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public string? ImageUrl { get; set; }
+		public string? Breed { get; set; }
+	}
+}
