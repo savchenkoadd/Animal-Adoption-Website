bb4a4188e367e2933454771fb83e0b242bfb879e
savchenkoadd <savchenkooad@gmail.com>
2023-11-26 19:00:46 +0200

    Initial

---
+++ AnimalAdoption/AnimalAdoption.UI/Program.cs
@@ -0,0 +1,13 @@
+namespace AnimalAdoption.UI
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
+
+			app.Run();
+		}
+	}
+}
