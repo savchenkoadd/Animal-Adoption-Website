84f02ecb0b0a21a8eb4f4027e9b3d46079c8cbc0
savchenkoadd <savchenkooad@gmail.com>
2023-11-26 19:44:19 +0200

    Implemented Animal service, changed the DTOs

--- AnimalAdoption/AnimalAdoption.Core/ServiceContracts/IAnimalService.cs
+++ AnimalAdoption/AnimalAdoption.Core/ServiceContracts/IAnimalService.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AnimalAdoption.Core.Domain.Entities;
+using AnimalAdoption.Core.DTO;
 
 namespace AnimalAdoption.Core.ServiceContracts
 {
-	internal interface IAnimalService
+	public interface IAnimalService
 	{
+		Task<bool> CreateAnimalProfile(AnimalProfileAddRequest? animalProfile);
+
+		Task<int> DeleteAnimalProfile(Guid? animalId);
+
+		Task<int> UpdateAnimalProfile(Guid? id, AnimalProfileUpdateRequest? animalRequest);
+
+		Task<IEnumerable<AnimalProfileResponse>> GetAnimalProfiles();
 	}
 }
---
+++ AnimalAdoption/AnimalAdoption.Core/Services/AnimalService.cs
@@ -0,0 +1,46 @@
+﻿using AnimalAdoption.Core.Domain.RepositoryContracts;
+using AnimalAdoption.Core.DTO;
+using AnimalAdoption.Core.ServiceContracts;
+using AnimalAdoption.Core.Domain.Entities;
+
+namespace AnimalAdoption.Core.Services
+{
+	public class AnimalService : IAnimalService
+	{
+		private IAnimalRepository _animalRepository;
+
+        public AnimalService(
+				IAnimalRepository animalRepository
+			)
+        {
+            _animalRepository = animalRepository;
+        }
+
+        public Task<bool> CreateAnimalProfile(AnimalProfileAddRequest? animalProfileRequest)
+		{
+			if (animalProfileRequest is null)
+			{
+				throw new ArgumentNullException();
+			}
+
+			var animalProfile1 = new AnimalProfile
+
+			_animalRepository.CreateAnimalProfile(animalProfileRequest);
+		}
+
+		public Task<int> DeleteAnimalProfile(Guid? animalId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<AnimalProfileResponse>> GetAnimalProfiles()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> UpdateAnimalProfile(AnimalProfileUpdateRequest? animalId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
