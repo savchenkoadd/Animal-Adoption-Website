fe35380c38f82a797dd68a44533fda36ffa02fef
savchenkoadd <savchenkooad@gmail.com>
2023-12-02 07:33:30 +0200

    Implement the request repository

---
+++ AnimalAdoption/src/AnimalAdoption.Core/Domain/RepositoryContracts/IRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using AnimalAdoption.Core.Domain.Entities;
+
+namespace AnimalAdoption.Core.Domain.RepositoryContracts
+{
+	public interface IRequestRepository
+	{
+		Task<bool> AddRequest(AnimalProfile request);
+
+		Task<bool> DeleteRequest(Guid requestId);
+	}
+}
---
+++ AnimalAdoption/src/AnimalAdoption.Infrastructure/Repositories/RequestRepository.cs
@@ -0,0 +1,41 @@
+﻿using AnimalAdoption.Core.Domain.Entities;
+using AnimalAdoption.Core.Domain.RepositoryContracts;
+using AnimalAdoption.Infrastructure.Db;
+
+namespace AnimalAdoption.Infrastructure.Repositories
+{
+	public class RequestRepository : IRequestRepository
+	{
+		private readonly ApplicationDbContext _db;
+
+		public RequestRepository(
+				ApplicationDbContext applicationDbContext
+			)
+		{
+			_db = applicationDbContext;
+		}
+
+		public async Task<bool> AddRequest(AnimalProfile request)
+		{
+			await _db.Requests.AddAsync(request);
+			await _db.SaveChangesAsync();
+
+			return true;
+		}
+
+		public async Task<bool> DeleteRequest(Guid requestId)
+		{
+			var animalProfile = await _db.Requests.FindAsync(requestId);
+
+			if (animalProfile is null)
+			{
+				return false;
+			}
+
+			_db.Requests.Remove(animalProfile);
+			await _db.SaveChangesAsync();
+
+			return true;
+		}
+	}
+}
