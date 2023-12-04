74c1de8aaba53b421de0e70fdad1a2b4cfc408fb
savchenkoadd <savchenkooad@gmail.com>
2023-12-04 08:49:09 +0200

    Create ContactController.cs

---
+++ AnimalAdoption/src/AnimalAdoption.UI/Controllers/ContactController.cs
@@ -0,0 +1,68 @@
+﻿using AnimalAdoption.Core.DTO;
+using AnimalAdoption.Core.Enums;
+using AnimalAdoption.Core.ServiceContracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalAdoption.UI.Controllers
+{
+	[Route("[controller]")]
+	public class ContactController : Controller
+	{
+		private readonly IContactService _contactService;
+
+		public ContactController(
+			IContactService contactService
+			)
+		{
+			_contactService = contactService;
+		}
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IActionResult> UserRequests()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		[Route("[action]")]
+		public async Task<IActionResult> Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Route("[action]")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(ContactFormRequest? contactFormRequest)
+		{
+			return View();
+		}
+
+		[HttpGet]
+		[Authorize(Roles = $"{nameof(UserTypeOptions.Admin)}")]
+		[Route("[action]")]
+		public async Task<IActionResult> Requests()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		[Authorize(Roles = $"{nameof(UserTypeOptions.Admin)}")]
+		[Route("[action]")]
+		public async Task<IActionResult> Respond()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[Authorize(Roles = $"{nameof(UserTypeOptions.Admin)}")]
+		[Route("[action]")]
+		public async Task<IActionResult> Respond(ContactFormRequest? contactFormRequest)
+		{
+			return View();
+		}
+	}
+}
