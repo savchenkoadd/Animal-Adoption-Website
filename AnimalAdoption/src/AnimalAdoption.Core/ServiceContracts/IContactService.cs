0bca99c293c6e879f366d38efc49feaa261d72cc
savchenkoadd <savchenkooad@gmail.com>
2023-12-04 08:34:28 +0200

    Create IContactService.cs

---
+++ AnimalAdoption/src/AnimalAdoption.Core/ServiceContracts/IContactService.cs
@@ -0,0 +1,16 @@
+﻿using AnimalAdoption.Core.Domain.Entities;
+using AnimalAdoption.Core.DTO;
+
+namespace AnimalAdoption.Core.ServiceContracts
+{
+	public interface IContactService
+	{
+		Task<bool> Create(ContactFormRequest contactFormRequest);
+
+		Task<List<ContactFormResponse>> GetAll();
+
+		Task<List<ContactFormResponse>> GetByUserId(Guid userId);
+
+		Task<bool> Respond(Guid userId, string response);
+	}
+}
