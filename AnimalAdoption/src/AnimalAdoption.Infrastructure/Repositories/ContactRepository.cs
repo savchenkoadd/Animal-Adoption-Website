fea8dfcaefe8eed2f03e547e7dcb783b01245fde
savchenkoadd <savchenkooad@gmail.com>
2023-12-04 08:55:08 +0200

    Implement ContactRepository

--- AnimalAdoption/src/AnimalAdoption.Infrastructure/Repositories/ContactRepository.cs
+++ AnimalAdoption/src/AnimalAdoption.Infrastructure/Repositories/ContactRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AnimalAdoption.Core.Domain.Entities;
 using AnimalAdoption.Core.Domain.RepositoryContracts;
 using AnimalAdoption.Infrastructure.Db;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalAdoption.Infrastructure.Repositories
 {
@@ -15,24 +16,39 @@ namespace AnimalAdoption.Infrastructure.Repositories
             _db = applicationDbContext;
         }
 
-        public Task<bool> Create(ContactForm contactForm)
+        public async Task<bool> Create(ContactForm contactForm)
 		{
-			throw new NotImplementedException();
+			await _db.ContactForms.AddAsync(contactForm);
+			await _db.SaveChangesAsync();
+
+			return true;
 		}
 
-		public Task<List<ContactForm>> GetAll()
+		public async Task<List<ContactForm>?> GetAll()
 		{
-			throw new NotImplementedException();
+			return await _db.ContactForms.ToListAsync();
 		}
 
-		public Task<List<ContactForm>> GetByUserId(Guid userId)
+		public async Task<List<ContactForm>> GetByUserId(Guid userId)
 		{
-			throw new NotImplementedException();
+			return await _db.ContactForms.Where(temp => temp.SenderId == userId).ToListAsync();
 		}
 
-		public Task<bool> Respond(Guid userId, string response)
+		public async Task<bool> Respond(Guid userId, Guid formId, string response)
 		{
-			throw new NotImplementedException();
+			var form = await _db.ContactForms.Where(temp => temp.SenderId == userId && temp.Id == formId).FirstOrDefaultAsync();
+
+			if (form is null)
+			{
+				return false;
+			}
+
+			form.Response = response;
+
+			_db.Update(form);
+			await _db.SaveChangesAsync();
+
+			return true;
 		}
 	}
 }
