a29b5e5998f69c59004f98b68e5f68c9359c4cce
savchenkoadd <savchenkooad@gmail.com>
2023-12-04 08:31:02 +0200

    Create required DTOs

---
+++ AnimalAdoption/src/AnimalAdoption.Core/DTO/ContactFormRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAdoption.Core.DTO
+{
+	public class ContactFormRequest
+	{
+		[Required(ErrorMessage = "SenderId can't be blank")]
+		public Guid SenderId { get; set; }
+
+		[StringLength(50, ErrorMessage = "Sender email must not exceed 50 characters")]
+		[EmailAddress(ErrorMessage = "Email must be valid")]
+		[Required(ErrorMessage = "Sender email can't be blank")]
+		public string? SenderEmail { get; set; }
+
+		[StringLength(50, ErrorMessage = "Subject must not exceed 50 characters")]
+		[Required(ErrorMessage = "Subject can't be blank")]
+		public string? Subject { get; set; }
+
+		[StringLength(1500, ErrorMessage = "Description must not exceed 1500 characters")]
+		[Required(ErrorMessage = "Description can't be blank")]
+		public string? Description { get; set; }
+	}
+}
---
+++ AnimalAdoption/src/AnimalAdoption.Core/DTO/ContactFormResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimalAdoption.Core.DTO
+{
+	public class ContactFormResponse
+	{
+		[Required(ErrorMessage = "SenderId can't be blank")]
+		public Guid SenderId { get; set; }
+
+		[StringLength(1500, ErrorMessage = "Response must not exceed 2000 characters")]
+		[Required(ErrorMessage = "Response can't be blank")]
+		public string? Response { get; set; }
+	}
+}
