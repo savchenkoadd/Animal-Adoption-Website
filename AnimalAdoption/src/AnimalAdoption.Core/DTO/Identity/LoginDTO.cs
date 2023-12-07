6384ad04396b2b15b52541553a7994b7281c0108
savchenkoadd <savchenkooad@gmail.com>
2023-12-07 11:01:09 +0200

    Documented the core layer

--- AnimalAdoption/src/AnimalAdoption.Core/DTO/Identity/LoginDTO.cs
+++ AnimalAdoption/src/AnimalAdoption.Core/DTO/Identity/LoginDTO.cs
@@ -2,13 +2,22 @@
 
 namespace AnimalAdoption.Core.DTO.Identity
 {
+	/// <summary>
+	/// Represents a data transfer object for user login information.
+	/// </summary>
 	public class LoginDTO
 	{
+		/// <summary>
+		/// Gets or sets the email address of the user.
+		/// </summary>
 		[Required(ErrorMessage = "Email cannot be blank")]
 		[EmailAddress(ErrorMessage = "Email should be valid")]
-		[DataType(DataType.Password)]	
+		[DataType(DataType.EmailAddress)]
 		public string? Email { get; set; }
 
+		/// <summary>
+		/// Gets or sets the password for user authentication.
+		/// </summary>
 		[Required(ErrorMessage = "Password cannot be blank")]
 		[DataType(DataType.Password)]
 		public string? Password { get; set; }
